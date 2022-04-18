20fa3a52bf2dda17b557960325cb5dd154cbcb31
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-19 02:30:19 +0430

    Initialize API Project

---
+++ API/Controllers/MetricsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace API;
+
+[PlanningArea]
+public class MetricsController : ApiController
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateMetric request, CancellationToken cancellationToken = default)
+        => Ok(await Mediator.Send(request, cancellationToken));
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateMetric request, CancellationToken cancellationToken = default)
+        => Ok(await Mediator.Send(request, cancellationToken));
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromQuery] DeleteMetric request, CancellationToken cancellationToken = default)
+        => Ok(await Mediator.Send(request, cancellationToken));
+}
---
+++ API/Program.cs
@@ -0,0 +1,29 @@
+using Repository.MongoDb;
+using Service;
+using Service.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMongoDbIdentification(builder.Configuration.GetSection("Identification:MongoDB").Get<MongoDbOptions>());
+builder.Services.AddMongoDbPlanning(builder.Configuration.GetSection("Planning:MongoDB").Get<MongoDbOptions>());
+
+builder.Services.AddJwtProvider(builder.Configuration.GetSection("Jwt").Get<JwtAuthenticationOptions>());
+
+builder.Services.AddRequiredServices();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
