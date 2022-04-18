b0f0aedc7c2a348627a78c91edf852d2ef23c2fd
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-19 02:29:29 +0430

    Add ServiceCollectionExtensions of Service.Jwt

---
+++ Service.Jwt/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Service.Jwt;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddJwtProvider(this IServiceCollection services, JwtAuthenticationOptions options)
+    {
+        services.AddSingleton(options);
+
+        services.AddScoped<IAuthenticationProvider, JwtAuthenticationProvider>();
+        services.AddScoped<IJwtClaimsProvider, JwtClaimsProvider>();
+
+        return services;
+    }
+}
