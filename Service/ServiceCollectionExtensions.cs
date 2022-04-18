dea93a0cd8705dc66ad1256a16ed64303431c957
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-19 02:29:48 +0430

    Add ServiceCollectionExtensions of Service

---
+++ Service/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Service;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRequiredServices(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
