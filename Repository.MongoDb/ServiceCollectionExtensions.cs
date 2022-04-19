6aef8ef0aa2bde457f12084c5c090304bde42939
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-19 05:27:35 +0430

    Implement BSON Serializers

--- Repository.MongoDb/ServiceCollectionExtensions.cs
+++ Repository.MongoDb/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using Domain;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using Repository;
 using Repository.MongoDb;
 
@@ -6,6 +9,24 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddDefaultBsonSerializers(this IServiceCollection services)
+    {
+        BsonDefaults.GuidRepresentationMode = GuidRepresentationMode.V3;
+        BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(GuidRepresentation.Standard));
+        BsonSerializer.RegisterSerializer(typeof(decimal), new DecimalSerializer(BsonType.Decimal128));
+        BsonSerializer.RegisterSerializer(typeof(decimal?), new NullableSerializer<decimal>(new DecimalSerializer(BsonType.Decimal128)));
+
+        return services;
+    }
+
+    public static IServiceCollection AddUserContextBsonSerializer<TUserContext>(this IServiceCollection services)
+        where TUserContext : IUserContext
+    {
+        BsonSerializer.RegisterSerializer(typeof(IUserContext), new UserContextBsonSerializer<TUserContext>());
+
+        return services;
+    }
+
     public static IServiceCollection AddMongoDbIdentification(this IServiceCollection services, MongoDbOptions options)
     {
         services.AddSingleton(new MongoDbOptions<IdentificationMongoDbContext>
