4dfe98d54d56a69150208428c62a8b84651dd0a6
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-24 14:20:47 +0430

    Review Entities

--- Core/Domain/Planning/Entities/Plan.cs
+++ Core/Domain/Planning/Entities/Plan.cs
@@ -8,7 +8,7 @@ public class Plan : Entity, IHasOwner
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
 
-    public virtual IEnumerable<Target> Targets { get; set; }
+    public virtual IEnumerable<object> TargetIds { get; set; }
 
     public string OwnerUserName { get; set; }
 }
--- Core/Domain/Planning/Entities/Target.cs
+++ Core/Domain/Planning/Entities/Target.cs
@@ -5,9 +5,9 @@ public class Target : Entity, IHasOwner
     public string Title { get; set; }
     public string Description { get; set; }
 
-    public virtual Plan Plan { get; set; }
+    public virtual object PlanId { get; set; }
+    public virtual object MetricId { get; set; }
 
-    public virtual Metric Metric { get; set; }
     public double Progress { get; set; }
 
     public string OwnerUserName { get; set; }
