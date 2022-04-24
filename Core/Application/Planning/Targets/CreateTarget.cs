aa6658062dde0417c5fdde6822cab863803dec2d
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-24 14:40:04 +0430

    Add PlanId, MetricId to CreateTarget

--- Core/Application/Planning/Targets/CreateTarget.cs
+++ Core/Application/Planning/Targets/CreateTarget.cs
@@ -4,6 +4,9 @@ public class CreateTarget : IRequest
 {
     public string Title { get; set; }
     public string Description { get; set; }
+
+    public object PlanId { get; set; }
+    public object MetricId { get; set; }
 }
 
 public class CreateTargetHandler : CreateRequestHandler<CreateTarget, Target>
