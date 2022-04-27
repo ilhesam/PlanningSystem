16dd31d164633d3f2fdd89e1ac6bebd5d3e809fd
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-27 11:19:41 +0430

    String IP Address

--- Presentation/API/Common/UserContext.cs
+++ Presentation/API/Common/UserContext.cs
@@ -1,15 +1,13 @@
-﻿using System.Net;
-
-namespace API;
+﻿namespace API;
 
 public class UserContext : IUserContext
 {
     public UserContext(IHttpContextAccessor accessor)
     {
         UserName = accessor.HttpContext?.User.Identity?.Name;
-        IpAddress = accessor.HttpContext?.Connection?.RemoteIpAddress;
+        IpAddress = accessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
     }
 
     public string UserName { get; set; }
-    public IPAddress IpAddress { get; set; }
+    public string IpAddress { get; set; }
 }
