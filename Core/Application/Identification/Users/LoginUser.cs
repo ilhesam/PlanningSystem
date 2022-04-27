fbb3d7f2ff6dc30fc3c31dcbb22ebddd6c3224be
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-27 09:39:56 +0430

    Implement Password Hashing on Register and Login

--- Core/Application/Identification/Users/LoginUser.cs
+++ Core/Application/Identification/Users/LoginUser.cs
@@ -18,17 +18,22 @@ public class LoginUserHandler : IRequestHandler<LoginUser, LoginUserResponse>
 {
     protected readonly IUserRepository Repository;
     protected readonly IAuthenticationProvider AuthenticationProvider;
+    protected readonly IPasswordHasher PasswordHasher;
 
-    public LoginUserHandler(IUserRepository repository, IAuthenticationProvider authenticationProvider)
+    public LoginUserHandler(IUserRepository repository, IAuthenticationProvider authenticationProvider, IPasswordHasher passwordHasher)
     {
         Repository = repository;
         AuthenticationProvider = authenticationProvider;
+        PasswordHasher = passwordHasher;
     }
 
     public async Task<LoginUserResponse> Handle(LoginUser request, CancellationToken cancellationToken)
     {
         var user = await Repository.GetByUserNameAsync(request.UserName, cancellationToken);
-        if (user.Password != request.Password) throw new Exception("User is not defined");
+
+        var passwordResult = PasswordHasher.Verify(request.Password, user.Password);
+        if (!passwordResult.Verified) throw new Exception("User is not defined");
+
         var credential = AuthenticationProvider.IssueCredential(user);
         return credential.MapTo<AuthenticationCredential, LoginUserResponse>();
     }
--- Core/Application/Identification/Users/RegisterUser.cs
+++ Core/Application/Identification/Users/RegisterUser.cs
@@ -8,7 +8,16 @@ public class RegisterUser : IRequest
 
 public class RegisterUserHandler : CreateRequestHandler<RegisterUser, User>
 {
-    public RegisterUserHandler(IUserRepository repository, IUserContext userContext) : base(repository, userContext)
+    protected readonly IPasswordHasher PasswordHasher;
+
+    public RegisterUserHandler(IUserRepository repository, IUserContext userContext, IPasswordHasher passwordHasher) : base(repository, userContext)
     {
+        PasswordHasher = passwordHasher;
+    }
+
+    public override async Task<Unit> Handle(RegisterUser request, CancellationToken cancellationToken)
+    {
+        request.Password = PasswordHasher.Hash(request.Password);
+        return await base.Handle(request, cancellationToken);
     }
 }
