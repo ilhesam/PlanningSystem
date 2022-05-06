c3c27c4ae03d4543f2b98920e95c5f2e2f1e5eed
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-06 14:55:19 +0430

    Add BasePageModel and UserContext

---
+++ Presentation/RazorPages/Common/BasePageModel.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages;
+
+public abstract class BasePageModel : PageModel
+{
+    private IMediator _mediator;
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+}
---
+++ Presentation/RazorPages/Common/UserContext.cs
@@ -0,0 +1,13 @@
+﻿namespace RazorPages;
+
+public class UserContext : IUserContext
+{
+    public UserContext(IHttpContextAccessor accessor)
+    {
+        UserName = accessor.HttpContext?.User.Identity?.Name;
+        IpAddress = accessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+    }
+
+    public string UserName { get; set; }
+    public string IpAddress { get; set; }
+}
